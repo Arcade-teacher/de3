e7a03f9ee8e97fa343ceb07cdbb0078dfe7faf26
Arcade-teacher <62038860+Arcade-teacher@users.noreply.github.com>
2020-09-01 22:34:06 +1000

    Added fish sprite

--- main.ts
+++ main.ts
@@ -97,3 +97,21 @@ let Monkey = sprites.create(img`
     . f d d f e e f d d b f f f f . 
     . f f f f f f f f f f f f f . . 
     `, SpriteKind.Player)
+let Fish = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . c c c c . . . 
+    . . . . . . . c c d d d d c . . 
+    . . . . . c c c c c c d d c . . 
+    . . . c c c 4 4 4 4 d c c c c c 
+    . . c 4 4 1 4 4 4 4 4 1 c c 4 f 
+    . c 4 4 4 4 1 4 4 4 4 d 1 f 4 f 
+    f 4 4 4 4 4 1 4 4 4 4 4 1 f 4 f 
+    f 4 4 f 4 4 1 4 c f 4 4 1 4 4 f 
+    f 4 4 4 4 4 1 c 4 f 4 4 1 f f f 
+    . f 4 4 4 4 1 4 4 f 4 4 d f . . 
+    . . f 4 4 1 4 c c 4 4 d f . . . 
+    . . . f d 4 4 4 4 4 4 c f . . . 
+    . . . . f f 4 4 4 4 c d b c . . 
+    . . . . . . f f f f d d d c . . 
+    . . . . . . . . . . c c c . . . 
+    `, SpriteKind.Player)
